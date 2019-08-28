b2073c3eb5b82b8c86c5149ed5d78272ad273bce
Nate Kwapo <48810912+mrkwapo@users.noreply.github.com>
2019-08-27 20:16:45 -0400

    Create people.js

---
+++ people.js
@@ -0,0 +1,63 @@
+// Object oriented programming exercise
+
+class Person {
+  constructor(name, age, height) {
+    this.name = name;
+    this.age = age;
+    this.height = height;
+    this.planet = 'earth';
+  }
+
+  changeName(newName) {
+    this.name = newName;
+  }
+
+  birthday() {
+    this.age += 1;
+  }
+
+  addHeight(height) {
+    this.height = height;
+  }
+}
+
+// const nate = new Person('Nate', 35, 180);
+// nate.addHeight(180);
+// console.log(nate.name, nate.age);
+// console.log(nate.height);
+
+// nate.birthday();
+// console.log(
+//   nate.name +
+//     ' is ' +
+//     nate.height +
+//     ' tall and he just turned ' +
+//     nate.age +
+//     '!!!'
+// );
+
+class Employee extends Person {
+  constructor(name, age, height) {
+    super(name, age, height);
+  }
+}
+
+// const newEmployee = new Employee('Nate', 30, 190);
+// console.log(newEmployee.name);
+// console.log(newEmployee.age);
+// console.log(newEmployee.height);
+// console.log(newEmployee.planet);
+
+class Student extends Person {
+  constructor(name, age, height, id) {
+    super(name, age, height);
+    this.id = id;
+  }
+}
+
+const newStudent = new Student('Amy', 35, 150, 455);
+console.log('Name:', newStudent.name);
+console.log('Age:', newStudent.age);
+console.log('Origin:', newStudent.planet);
+console.log('Height:', newStudent.height);
+console.log('ID #:', newStudent.id);
